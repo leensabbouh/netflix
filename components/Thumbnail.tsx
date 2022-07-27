2e5eec93418a3d0b8dbdc9f402d74b4c4f2a3b58
leensabbouh <leensabbouh9@gmail.com>
2022-07-27 13:42:37 -0400

    m

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,24 +1,26 @@
-import React from 'react'
+import { DocumentData } from 'firebase/firestore'
 import Image from 'next/image'
-import { Movie } from '../typing'
 import { useRecoilState } from 'recoil'
 import { modalState, movieState } from '../atoms/modalAtom'
-import { DocumentData } from 'firebase/firestore'
-interface Props{
-  
-  movie:Movie | DocumentData[]
+import { Movie } from '../typing'
+
+interface Props {
+  movie: Movie | DocumentData
 }
-function Thumbnail({movie}:Props) {
-  
-  const [showModal,setShowModal]=useRecoilState(modalState)
-  const[currentMovie,setCurrentMovie]= useRecoilState(movieState)
+
+function Thumbnail({ movie }: Props) {
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+  const [showModal, setShowModal] = useRecoilState(modalState)
 
   return (
-    <div className='relative h028 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105' onClick={()=>{
-      setCurrentMovie(movie)
-      setShowModal(true)
-    }}>
-       <Image
+    <div
+      className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105`}
+      onClick={() => {
+        setCurrentMovie(movie)
+        setShowModal(true)
+      }}
+    >
+      <Image
         src={`https://image.tmdb.org/t/p/w500${
           movie.backdrop_path || movie.poster_path
         }`}
